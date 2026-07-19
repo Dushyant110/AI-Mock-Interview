356f06f88db2b2b940004502780d5340295aedc6
Dushyant110 <dushyant.sharma.0111@gmail.com>
2026-07-20 03:43:25 +0530

    interview input created

--- client/src/components/Step1SetUp.jsx
+++ client/src/components/Step1SetUp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   FaUserTie,
   FaBriefcase,
@@ -10,7 +10,9 @@ import { motion } from "framer-motion";
 
 function Step1SetUp({ onStart }) {
 
-  
+  const [role, setRole] = useState("");
+  const [experience, setExperience] = useState("");
+  const [mode, setMode] = useState("Technical");
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -66,8 +68,39 @@ function Step1SetUp({ onStart }) {
 
         </motion.div>
 
-        <motion.div>
-          {/* Right Section */}
+        <motion.div
+          initial={{ x: 80, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.7 }}
+          className='p-12 bg-white'>
+          <h2 className='text-3xl font-bold text-gray-800 mb-8'>
+            Interview SetUp
+          </h2>
+
+          <div className='space-y-6'>
+            <div className='relative'>
+              <FaUserTie className='absolute top-4 left-4 text-gray-400' />
+
+              <input
+                type='text'
+                placeholder='Enter role'
+                className='w-full pl-12 pr-4 py-3 border border-gray-200 rounded-xl focus:ring-2 focus:ring-green-500 outline-none transition'
+                onChange={(e) => setRole(e.target.value)} value={role}
+              />
+            </div>
+
+            <div className='relative'>
+              <FaBriefcase className='absolute top-4 left-4 text-gray-400' />
+
+              <input
+                type='text'
+                placeholder='Experience (e.g. 2 years)'
+                className='w-full pl-12 pr-4 py-3 border border-gray-200 rounded-xl focus:ring-2 focus:ring-green-500 outline-none transition'
+                onChange={(e) => setExperience(e.target.value)} value={experience}
+              />
+            </div>
+          </div>
+
         </motion.div>
       </div>
     </motion.div>
