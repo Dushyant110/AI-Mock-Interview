93488a287488e018167892aa927cb83887edc3d5
Dushyant110 <dushyant.sharma.0111@gmail.com>
2026-07-20 01:33:43 +0530

    interview router

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ dotenv.config()
 import cors from "cors"
 import authRouter from "./routes/auth.route.js"
 import userRouter from "./routes/user.route.js"
+import interviewRouter from "./routes/interview.route.js"
 
 const app = express()
 
@@ -24,6 +25,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
+app.use("/api/interview", interviewRouter)
 
 const PORT = process.env.PORT || 6000
 app.listen(PORT, () =>{
