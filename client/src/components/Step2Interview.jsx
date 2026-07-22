9dbf9c16adc193a097b90f0a02ffdcdb902ef8bd
Dushyant110 <dushyant.sharma.0111@gmail.com>
2026-07-23 00:55:07 +0530

    step2setup

--- client/src/components/Step2Interview.jsx
+++ client/src/components/Step2Interview.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import maleVideo from "../assets/Videos/male-ai.mp4"
 import femaleVideo from "../assets/Videos/female-ai.mp4"
 import Timer from './Timer'
+import { motion } from 'motion/react'
+import { FaMicrophone, FaMicrophoneSlash } from 'react-icons/fa'
 
 function Step2Interview({ interviewData, onFinish }) {
 
@@ -60,6 +62,46 @@ function Step2Interview({ interviewData, onFinish }) {
 
           </div>
         </div>
+
+
+        {/* Text section */}
+        <div className="flex-1 flex flex-col p-4 sm:p-6 md:p-8 relative">
+          <h2 className="text-xl sm:text-2xl font-bold text-emerald-600 mb-6">
+            AI Smart Interview
+          </h2>
+
+          <div className="relative mb-6 bg-gray-50 p-4 sm:p-6 rounded-2xl border border-gray-200 shadow-sm">
+            <p className="text-xs sm:text-sm text-gray-400 mb-2">
+              Question 1 of 5
+            </p>
+
+            <div className="text-base sm:text-lg font-semibold text-gray-800 leading-relaxed">
+              First Question
+            </div>
+          </div>
+
+          <textarea
+            placeholder="Type your answer here..."
+            className="flex-1 bg-gray-100 p-4 sm:p-6 rounded-2xl resize-none outline-none border border-gray-200 focus:ring-2 focus:ring-emerald-500 transition text-gray-800"
+          />
+
+          <div className="flex items-center gap-4 mt-6">
+            <motion.button
+              whileTap={{ scale: 0.9 }}
+              className="w-12 h-12 sm:w-14 sm:h-14 flex items-center justify-center rounded-full bg-black text-white shadow-lg"
+            >
+              <FaMicrophone size={20} />
+            </motion.button>
+
+            <motion.button
+              whileTap={{ scale: 0.95 }}
+              className="flex-1 bg-gradient-to-r from-emerald-600 to-teal-500 text-white py-3 sm:py-4 rounded-2xl shadow-lg hover:opacity-90 transition font-semibold"
+            >
+              Submit Answer
+            </motion.button>
+          </div>
+        </div>
+
       </div>
     </div>
   )
