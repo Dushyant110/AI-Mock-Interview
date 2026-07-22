b964898ad6fb7ad2c3951a89ff79ae5d50eda6bf
Dushyant110 <dushyant.sharma.0111@gmail.com>
2026-07-22 23:21:35 +0530

    Timer added

--- client/src/components/Step2Interview.jsx
+++ client/src/components/Step2Interview.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import maleVideo from "../assets/Videos/male-ai.mp4"
 import femaleVideo from "../assets/Videos/female-ai.mp4"
+import Timer from './Timer'
 
 function Step2Interview({ interviewData, onFinish }) {
 
-  const { interviewId, questions, userName } = interviewData
+  // const { interviewId, questions, userName } = interviewData
 
   return (
     <div className='min-h-screen bg-linear-to-br from-emerald-50 via-white to-teal-100 flex items-center justify-center p-4 sm:p-6'>
@@ -23,6 +24,26 @@ function Step2Interview({ interviewData, onFinish }) {
           </div>
 
           {/* subtitle */}
+
+
+          {/* timer Area */}
+          <div className="w-full max-w-md bg-white border border-gray-200 rounded-2xl shadow-md p-6 space-y-5">
+            <div className="flex justify-between items-center">
+              <span className="text-sm text-gray-500">
+                Interview Status
+              </span>
+              <span className="text-sm font-semibold text-emerald-600">
+                AI Speaking
+              </span>
+            </div>
+
+            <div className="h-px bg-gray-200"></div>
+
+            <div className='flex justify-center'>
+              <Timer timeLeft="30" totalTime="60" />
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
