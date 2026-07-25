64467acfb672f4ec1069b7499fcbf49f0cbdba2e
Dushyant110 <dushyant.sharma.0111@gmail.com>
2026-07-25 18:06:04 +0530

    payment router

--- server/index.js
+++ server/index.js
@@ -7,28 +7,24 @@ import cors from "cors"
 import authRouter from "./routes/auth.route.js"
 import userRouter from "./routes/user.route.js"
 import interviewRouter from "./routes/interview.route.js"
+import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
-
 app.use(cors({
     origin:"http://localhost:5173",
-    credentials: true,
+    credentials:true
 }))
-
 
 app.use(express.json())
 app.use(cookieParser())
 
-app.get("/", (req, res) => {
-    res.send("Server is running");
-});
-
-app.use("/api/auth", authRouter)
+app.use("/api/auth" , authRouter)
 app.use("/api/user", userRouter)
-app.use("/api/interview", interviewRouter)
+app.use("/api/interview" , interviewRouter)
+app.use("/api/payment" , paymentRouter)
 
 const PORT = process.env.PORT || 6000
-app.listen(PORT, () =>{
-    console.log(`Serever is running on port ${PORT}`)
+app.listen(PORT , ()=>{
+    console.log(`Server running on port ${PORT}`)
     connectDb()
 })
