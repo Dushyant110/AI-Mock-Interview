8defc6abbcbe829458cadf1be75953750fafc50e
Dushyant110 <dushyant.sharma.0111@gmail.com>
2026-07-26 18:41:05 +0530

    Update multer upload path

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,16 +1,27 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+// Create uploads folder if it doesn't exist
+const uploadDir = path.join(process.cwd(), "uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,"public")
-    },
-    filename: function(req,file,cb){
-        const filename = Date.now() + "-" + file.originalname;
-        cb(null,filename)
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    const filename = Date.now() + "-" + file.originalname;
+    cb(null, filename);
+  },
+});
 
 export const upload = multer({
-    storage,
-    limits: {filesize : 5 * 1024 * 1024} // 5MB limit
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5 MB
+  },
 });
